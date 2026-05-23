d9b0439809bef55402424641e79bd5cf2070e23f
LuxUmbris <luxumbris@icloud.com>
2026-05-23 04:27:34 +0000

    temporary push build dir to debug

---
+++ build/CMakeFiles/cshift.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cshift.dir/main.cpp.o"
+  "CMakeFiles/cshift.dir/main.cpp.o.d"
+  "cshift"
+  "cshift.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cshift.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/frt_native.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for frt_native.
+# This may be replaced when dependencies are built.
