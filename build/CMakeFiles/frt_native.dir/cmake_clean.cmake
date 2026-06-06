0c923abfe06b5ccf530395e56f34e0fa069829b2
LuxUmbris <luxumbris@icloud.com>
2026-06-06 11:21:33 +0200

    Delete build directory

--- build/CMakeFiles/frt_native.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/frt_native"
-  "frt/native/frt.o"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/frt_native.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
